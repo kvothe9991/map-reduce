e578128e93af2f9fc8604b286e2e3de5b5993d0a
Carlos Aguila <kvothe9991@gmail.com>
2022-06-17 23:27:28 -0400

    Finished dht/chord/ChordNode
    testing, replication and multiple successors are still pending...

--- server/dht/utils.py
+++ server/dht/utils.py
@@ -1,11 +1,25 @@
 from hashlib import sha1
-from typing import Callable, Tuple
+from typing import Callable, Union
 
-HASH_BIT_COUNT = 160
+from Pyro4 import URI, Proxy
+from Pyro4.errors import CommunicationError
+
+BIT_COUNT = 160
 
 
-def id_from(key: str, hash: Callable = sha1) -> int:
-    ''' Returns a numerical identifier obtained from hashing a string key. '''
+def alive(proxy: Proxy):
+    try:
+        proxy._pyroBind()
+    except CommunicationError:
+        return False
+    finally:
+        return True
+
+def id(key: Union[str,URI], hash: Callable = sha1) -> int:
+    ''' Returns a numerical identifier obtained from hashing an string key.
+    Additional support for Pyro URIs for convenience. '''
+    if isinstance(key, URI):
+        key = key.host
     hex_hash = hash(key.encode()).hexdigest()
     id = int(hex_hash, base=16)
     return id
