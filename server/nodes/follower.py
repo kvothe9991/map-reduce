40483adc2ceaaf35316087c3140dd326f3c0cd2f
batheaded <batheaded@protonmail.com>
2022-06-18 12:44:20 -0400

    Reducer and Mapper

--- server/nodes/follower.py
+++ server/nodes/follower.py
@@ -6,8 +6,30 @@ class Follower(ThreaderNode):
     def __init__(self, address: URI) -> None:
         super().__init__(address)
 
-    def map(self, key, value):
+    def mapper(self, key, value):
+        mapper = Mapper(self._address, key, value)
         pass
 
-    def reduce(self, key, values_list):
+    def reducer(self, key, values_list):
+        reducer = Reducer(self._address, key, values_list)
+        pass
+
+
+class Mapper(ThreaderNode):
+    def __init__(self, address: URI, key, value) -> None:
+        super().__init__(address)
+        self._key = key
+        self._value = value
+
+    def run(self):
+        pass
+
+
+class Reducer(ThreaderNode):
+    def __init__(self, address: URI, key, values_list) -> None:
+        super().__init__(address)
+        self._key = key
+        self._values_list = values_list
+
+    def run(self):
         pass
