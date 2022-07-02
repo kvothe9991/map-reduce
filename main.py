cde538993fb8a62995460c45f97501b14774323a
Carlos Aguila <kvothe9991@gmail.com>
2022-07-02 18:03:38 -0400

    Added main, nameserver binder, daemon servings and multithreaded logic.

---
+++ main.py
@@ -0,0 +1,72 @@
+import logging
+import socket, threading
+import Pyro4
+import Pyro4.errors
+import Pyro4.naming
+import Pyro4.socketutil
+from Pyro4 import Proxy, URI
+Pyro4.config.SERVERTYPE = 'multiplex'
+Pyro4.config.POLLTIMEOUT = 3
+Pyro4.config.COMMTIMEOUT = 3
+
+from server.nameserver import BoundNameServer
+from server.dht import ChordNode, DHT_NAME
+
+HOSTNAME = socket.gethostname()
+IP = Pyro4.socketutil.getIpAddress(None, workaround127=True)
+DAEMON_PORT = 8008
+BROADCAST_PORT = 8009
+
+# Daemon objects.
+DAEMON = Pyro4.Daemon(host=IP, port=DAEMON_PORT)
+DHT = ChordNode(URI(f'PYRO:{DHT_NAME}@{IP}:{DAEMON_PORT}'))
+
+# URIs and Daemon registration.
+DHT_URI = DAEMON.register(DHT, DHT_NAME)
+
+# Nameserver.
+NS = BoundNameServer(IP, BROADCAST_PORT)
+NS_THREAD = threading.Thread(target=NS.check_NS_loop)
+NS_THREAD.setDaemon(True)
+NS_THREAD.start()
+
+
+
+
+if __name__ == '__main__':
+    
+    # Preface logging.
+    logger = logging.getLogger('[MAIN]')
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = 0
+    logger.info(f'Started with:\n\t{HOSTNAME=}\n\t{IP=}.')
+
+    with NS.bind() as ns:
+        try:
+            ring_uri = ns.lookup(DHT_NAME)
+            DHT.join(ring_uri)
+        except Pyro4.errors.NamingError:
+            ns.register(DHT_NAME, DHT_URI)
+            logger.info(f'No DHT found. Registered {DHT_NAME} at {DHT_URI}.')
+    
+    try:
+        DAEMON.requestLoop()
+    except KeyboardInterrupt:
+        logger.info('Server stopped. Exiting.')
+    finally:
+        logger.info('Killing nameserver thread.')
+        NS.kill_check_NS_loop()
+        if NS_THREAD.join(timeout=0.2) and NS_THREAD.is_alive():
+            logger.error('Nameserver thread did not stop after daemon shutdown order.')
+        
+        logger.info('Killing daemon.')
+        DAEMON.shutdown()
+
+        logger.info('Deleting garbage objects.')
+        del NS
+        del NS_THREAD
+        del DAEMON
+        del DHT
+        
+        logger.info('Exiting.')
+        exit(0)
--- server/nameserver/nameserver.py
+++ server/nameserver/nameserver.py
@@ -1,13 +1,17 @@
-import logging
+import logging, time
+from typing import Union
+from threading import Thread
+
 import Pyro4
 import Pyro4.errors
 import Pyro4.naming
-from typing import Union
 from Pyro4 import URI, Proxy
 from Pyro4.naming import NameServerDaemon, BroadcastServer
 
 from server.utils import alive, reachable, id
 from server.nameserver.logger import logger
+
+NAMESERVER_CHECK_INTERVAL = 5
 
 
 class BoundNameServer:
@@ -16,28 +20,31 @@ class BoundNameServer:
         self._ip = ip
         self._port = port
         self._quickname = f'PYRO:(ns.binder)@{self._ip}:{self._port}'
-        logger = logging.LoggerAdapter(logger, {'URI': self._quickname})
+        self.logger = logging.LoggerAdapter(logger, {'URI': self._quickname})
 
-        self.URI = None
-        self._ns_thread = None
-        self._ns_daemon = None
-        self._ns_broadcast = None
-        self._remote = False
+        self.uri: URI = None
+        self._ns_thread: Thread = None
+        self._ns_daemon: NameServerDaemon = None
+        self._ns_broadcast: BroadcastServer = None
+        self._keep_checking_loop = True
 
         # Initial search for nameserver.
         if uri := self.locate_NS():
-            logger.info(f'Found existing nameserver {uri}.')
-            self.URI = uri
-            self._remote = True
+            self.logger.info(f'Found existing nameserver {uri}.')
+            self.uri = uri
         else:
             self.start()
 
     def __str__(self):
         status = 'remote' if self._remote else 'local'
-        return f'BoundNameServer<{status}>@[{self.URI}]'
+        return f'BoundNameServer<{status}>@[{self.uri}]'
     
     def __repr__(self):
         return str(self)
+    
+    @property
+    def remote(self) -> bool:
+        return self._ns_thread is None or not self._ns_thread.is_alive()
 
     def locate_NS(self) -> Union[URI, None]:
         ''' Attempts to locate a remote nameserver. Returns its URI if found. '''
@@ -49,47 +56,66 @@ class BoundNameServer:
     
     def start(self):
         ''' Starts the local nameserver. '''
-        self.URI, self._ns_daemon, self._ns_broadcast = Pyro4.naming.startNS(
-            host=self._ip, port=self._port)
+        self.uri, self._ns_daemon, self._ns_broadcast = Pyro4.naming.startNS(self._ip,
+                                                                             self._port)
         self._ns_daemon.combine(self._ns_broadcast)
-        self._remote = False
-        logger.info(f'Local nameserver started at {self.URI}.')
+        
+        self._ns_thread = Thread(target=self._ns_daemon.requestLoop)
+        self._ns_thread.setDaemon(True)
+        self._ns_thread.start()
+        self.logger.info(f'Local nameserver started at {self.uri}.')
 
     def stop(self):
         ''' Stops the local nameserver. '''
         self._ns_daemon.shutdown()
-        self._remote = True
-        self.info(f'Local nameserver stopped.')
+        if self._ns_thread.join(0.1) and self._ns_thread.is_alive():
+            self.logger.error('Nameserver thread did not stop after daemon shutdown order.')
+        else:
+            self._ns_thread = None
+            self._ns_daemon = self._ns_broadcast = None
+            self.logger.info(f'Local nameserver stopped.')
     
-    def check_NS(self) -> tuple[bool, URI]:
+    def check_NS(self):
         ''' Checks for a nameserver in the network. Announces self otherwise. '''
-        if self._remote:
-            if reachable(self.URI):
-                logger.info(f'Remote nameserver {self.URI} is reachable as expected.')
+        if self.remote:
+            if reachable(self.uri):
+                self.logger.debug(f'Remote nameserver {self.uri} is reachable as expected.')
             else:
-                logger.warning(f'Remote nameserver {self.URI} is not reachable.')
+                self.logger.warning(f'Remote nameserver {self.uri} is not reachable.')
                 if new_uri := self.locate_NS():
-                    logger.info(f'Found new nameserver {new_uri}.')
-                    self.URI = new_uri
+                    self.logger.info(f'Found new nameserver {new_uri}.')
+                    self.uri = new_uri
                 else:
-                    logger.warning(f'No new nameserver found. Announcing self.')
+                    self.logger.warning(f'No new nameserver found. Announcing self.')
                     self.start()
         else:
-            if (new_uri := self.locate_NS()) is not None and new_uri != self.URI:
-                logger.info(f'Found contesting nameserver {new_uri}.')
-                if id(self.URI) < id(new_uri):
-                    logger.info(f'I am still the nameserver.')
+            if (new_uri := self.locate_NS()) is not None and new_uri != self.uri:
+                self.logger.info(f'Found contesting nameserver {new_uri}.')
+                if id(self.uri) < id(new_uri):
+                    self.logger.info(f'I am still the nameserver.')
                 else:
-                    logger.info(f'I no longer am the nameserver, long live {new_uri}.')
-                    self.URI = new_uri
+                    self.logger.info(f'I no longer am the nameserver, long live {new_uri}.')
+                    self.uri = new_uri
                     self.stop()
-        return (self._remote, self.URI)
 
-    def bind(self) -> Proxy:
+    def check_NS_loop(self):
+        '''
+        Higher level loop to run in a daemonized thread. Checks for a nameserver
+        and spawns one if there is none running in the network.  The
+        `self._keep_checking_loop` attribute is used to stop the loop.
+        '''
+        self.logger.info('Starting nameserver check loop.')
+        self._keep_checking_loop = True
+        while self._keep_checking_loop:
+            time.sleep(10)
+            self.check_NS()
+        self.stop()
+    
+    def kill_check_NS_loop(self):
+        ''' Kills high-level running loop `check_NS_loop` for when it is thread-bound. '''
+        self._keep_checking_loop = False
+
+    def bind(self) -> Pyro4.naming.NameServer:
         ''' Returns a proxy bound to the nameserver, local or remote. Should be used
         with a context manager. '''
-        return Proxy(self.URI)
-
-    def local_servers(self) -> tuple[URI, NameServerDaemon, BroadcastServer]:
-        ''' Returns a tuple of URI and both local raised servers by Pyro. '''
-        return self.URI, self._ns_daemon, self._ns_broadcast
+        return Proxy(self.uri)
